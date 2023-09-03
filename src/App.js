85d886e1e52a31bb810ae721a2e53839a6081f4a
Anshuman Shukla <Anshumanshukla46@gmail.com>
2023-09-03 10:18:28 +0530

    1-10

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+
+  const [isCount, setIsCount] = useState(false)
+
+  function startCount() {
+    let count = 1;
+
+    setIsCount(true)
+    const inter = setInterval(() => {
+      console.log(count);
+      count++;
+
+      if (count > 10) {
+        clearInterval(inter)
+        setIsCount(false);
+      }
+    }, 1000)
+
+
+  }
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Count</h1>
+      <button onClick={startCount}>I'm feeling Lucky</button>
+
+
+
     </div>
   );
 }
